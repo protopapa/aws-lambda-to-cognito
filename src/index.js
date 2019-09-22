19b499ad1149ffa88d0d9e2b07b71c2e353211eb
protopapa <natassa.prt@gmail.com>
2019-09-22 11:30:31 +0200

    WIP - async await

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 const aws = require('aws-sdk');
 
 const cognito = new aws.CognitoIdentityServiceProvider({apiVersion: 'latest', region: 'eu-west-1'});
-
 const userRequest = {
     UserPoolId: '',
     Username: '',
 };
-
 const response = {
     statusCode: 200,
     body: JSON.stringify({
@@ -14,32 +12,22 @@ const response = {
     }),
 };
 
-exports.handler = function (event, context, callback) {
+function adminDeleteUser(user) {
+    if (user && user['UserStatus'] === 'UNCONFIRMED') {
+        userRequest.Username = user.email;
+        return cognito.adminDeleteUser(userRequest)
+            .promise().catch(err => "Error on delete: " + JSON.stringify(err));
+    }
+}
+
+exports.handler = async function (event, context) {
     let body = JSON.parse(event.body);
     if (body.userPoolId) userRequest.UserPoolId = body.userPoolId;
     if (body.email) userRequest.Username = body.email;
 
-    cognito.adminGetUser(userRequest, function (getUserError, getUserData) {
-        if (!getUserError) {
-            if (getUserData['UserStatus'] === 'UNCONFIRMED') {
-                cognito.adminDeleteUser(userRequest, function (deleteError, deleteData) {
-                    if (!deleteError) {
-                    } else {
-                        console.log('deleteError: ' + JSON.stringify(deleteError));
-                    }
-                });
-            } else {
-                console.log('User is already confirmed. Skipping');
-            }
-        } else {
-            console.log('getUserError: ' + JSON.stringify(getUserError));
-        }
+    const user = await cognito.adminGetUser(userRequest).promise()
+        .catch(err => console.log("Error on: " + JSON.stringify(err)));
+    await adminDeleteUser(user);
 
-        callback(null, response);
-    });
-
-
-
-
-
+    return response;
 };
